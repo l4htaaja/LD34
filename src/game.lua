6a85a45329c25bcc33f1c1f0454dc3d9065aed62
l4htaaja <anssi_puustinen@hotmail.com>
2015-12-13 00:19:04 +0200

    Animations and sfx

--- src/game.lua
+++ src/game.lua
@@ -1,13 +1,15 @@
 local k = love.keyboard
 local g = love.graphics
+local img = love.image
 local m = love.mouse
+local a = love.audio
 local win = love.window
 local sti = require "lib.sti"
 
 game = {}
 
 function game:init()
-    g.setBackgroundColor(255, 255, 255)
+    g.setBackgroundColor(120, 125, 140)
 
     self.map = sti.new("assets/map.lua")
     self.map:addCustomLayer("GameObjects", 3)
@@ -17,8 +19,7 @@ function game:init()
     GOLayer.GameObjects = {    
         player = require "src.player"
     }
-    GOLayer.GameObjects.player.p.x = playerSpawnProperties.x
-    GOLayer.GameObjects.player.p.y = playerSpawnProperties.y
+    GOLayer.GameObjects.player:init(playerSpawnProperties.x, playerSpawnProperties.y)
 
     function GOLayer:draw()
         self.GameObjects.player:draw()
@@ -32,8 +33,29 @@ function game:init()
         window = {
             w = g.getWidth(),
             h = g.getHeight()
-        }
+        },
+        scale = 3
     }
+
+    self.audio = {
+        bg = a.newSource("assets/wind.ogg", "stream")
+    }
+    self.audio.bg:setLooping(true)
+    self.audio.bg:play()
+
+    local imgData = img.newImageData(4, 4)
+    for i = 0, 3, 1 do
+        for j = 0, 3, 1 do
+            imgData:setPixel(i, j, 150, 150, 150, 255)
+        end
+    end
+    self.snowParticles = g.newParticleSystem(g.newImage(imgData), 256)
+    self.snowParticles:setParticleLifetime(20, 30)
+    self.snowParticles:setEmissionRate(10)
+    self.snowParticles:setSpeed(50, 100)
+    self.snowParticles:setDirection(math.pi/2)
+    self.snowParticles:setAreaSpread("normal", 50*16, 0)
+    self.snowParticles:start()
 end
 
 -- Also entered, leaving and left
@@ -42,16 +64,19 @@ end
 
 function game:draw()
     g.push()
+        g.scale(self.settings.scale, self.settings.scale)
         local player = self.map.layers["GameObjects"].GameObjects.player
-        g.translate(-player.p.x + self.settings.window.w / 2, -player.p.y + self.settings.window.h / 2)
-        self.map:setDrawRange(player.p.x - self.settings.window.w / 2, player.p.y - self.settings.window.h / 2, self.settings.window.w, self.settings.window.h)
+        g.translate(-player.p.x + self.settings.window.w / (2 * self.settings.scale), -player.p.y + self.settings.window.h / (2 * self.settings.scale))
+        self.map:setDrawRange(player.p.x - self.settings.window.w / 2, player.p.y - self.settings.window.h / 2, self.settings.window.w, self.settings.window.h) 
         self.map:drawLayer(self.map.layers["Collision"])
         self.map:drawLayer(self.map.layers["GameObjects"])
+        g.draw(self.snowParticles, 50*16, 0)
     g.pop()
 end
 
 function game:update(dt)
     self.map:update(dt)
+    self.snowParticles:update(dt)
 end
 
 return game
--- src/player.lua
+++ src/player.lua
@@ -1,28 +1,66 @@
 local k = love.keyboard
 local g = love.graphics
 
+local anim8 = require "lib.anim8.anim8"
+
+local GRAVITY = 150
+local ACCELERATION = 400
+local DRAG = 10 -- Drag coefficient something something
+local CLIMB_ACCELERATION = -100
+local MAX_VELOCITY_X = 75
+local MAX_VELOCITY_Y = 150
+local SPRING_VELOCITY = -150
+
 return {
+    spritesheet = g.newImage("assets/player.png"),
+    currAnimation = nil,
+    anims = {
+    },
     d = {
         w = 8,
-        h = 14
+        h = 14,
+        sprite = {
+            w = 10,
+            h = 16
+        }
     },
     p = {
         x = 0,
         y = 0
-    },
+    }, 
+    direction = 1,
     v = {
         x = 0,
         y = 0
     },
     a = {
         x = 0,
-        y = 100 -- gravity
+        y = 0
     },
     state = {
         grounded = false,
         huggingLeft = false,
-        huggingRight = false
+        huggingRight = false,
+        climbing = false
     },
+    init = function(self, x, y)
+        self.p.x = x
+        self.p.y = y
+
+        self.spritesheet:setFilter("nearest", "nearest")
+        local grid = anim8.newGrid(self.d.sprite.w, self.d.sprite.h, self.spritesheet:getWidth(), self.spritesheet:getHeight())
+        self.anims.placeHolder = anim8.newAnimation(grid(7, 1), 999)
+        self.anims.idleLeft = anim8.newAnimation(grid("1-6", 1), 0.35)
+        self.anims.idleRight = anim8.newAnimation(grid("1-6", 2), 0.35)
+        self.anims.moveLeft = anim8.newAnimation(grid("1-5", 3), 0.15)
+        self.anims.moveRight = anim8.newAnimation(grid("1-5", 4), 0.15)
+        self.anims.risingLeft = anim8.newAnimation(grid(6, 3), 999)
+        self.anims.fallingLeft = anim8.newAnimation(grid(7, 3), 999)
+        self.anims.risingRight = anim8.newAnimation(grid(6, 4), 999)
+        self.anims.fallingRight = anim8.newAnimation(grid(7, 4), 999)
+        self.anims.climbingLeft = anim8.newAnimation(grid("1-4", 5), 0.2)
+        self.anims.climbingRight = anim8.newAnimation(grid("1-4", 6), 0.2)
+    end,
     update = function(self, map, dt)
         local isSpring = function(x, y)
             return map:getTileProperties("Collision", x + 1, y + 1).id == 1
@@ -44,10 +82,10 @@ return {
 
         -- Input
         self.a.x = 0
-        self.a.y = 100
+        self.a.y = GRAVITY
 
-        if k.isDown("left") then self.a.x = self.a.x - 100 end
-        if k.isDown("right") then self.a.x = self.a.x + 100 end
+        if k.isDown("left") then self.a.x = self.a.x - ACCELERATION end
+        if k.isDown("right") then self.a.x = self.a.x + ACCELERATION end
 
         -- Vertical movement
         local spring = self.state.grounded and (isSpring(lx, dy + 1) or isSpring(rx, dy + 1))
@@ -59,15 +97,45 @@ return {
         --
         -- These could very well be "active objects" too
         if spring then
-            self.v.y = -100
+            self.v.y = SPRING_VELOCITY
         end
 
+        self.state.climbing = false
         if (canClimbLeft and self.a.x < 0) or (canClimbRight and self.a.x > 0) then
-            self.a.y = -self.a.y
+            self.a.y = CLIMB_ACCELERATION
+            self.state.climbing = true
         end
-        
+       
+        -- Drag
+        if self.a.x == 0 then -- no input
+            if self.v.x > 1 then
+                self.a.x = -DRAG*self.v.x
+            elseif self.v.x < -1 then
+                self.a.x = -DRAG*self.v.x
+            else
+                self.a.x = 0
+                self.v.x = 0
+            end            
+        end
+
         self.v.x = self.v.x + self.a.x * dt
+        -- Cap speed (Should probably handle this through
+        -- "diminishing returns")
+        if math.abs(self.v.x) > MAX_VELOCITY_X then
+            if self.v.x < 0 then
+                self.v.x = -MAX_VELOCITY_X
+            else
+                self.v.x = MAX_VELOCITY_X
+            end
+        end
         self.v.y = self.v.y + self.a.y * dt
+        if math.abs(self.v.y) > MAX_VELOCITY_Y then
+            if self.v.y < 0 then
+                self.v.y = -MAX_VELOCITY_Y
+            else
+                self.v.y = MAX_VELOCITY_Y
+            end
+        end
 
         local newX = 0
         local newY = 0
@@ -88,6 +156,8 @@ return {
         self.state.huggingLeft = false
         self.state.huggingRight = false
         if self.v.x < 0 then
+            self.direction = -1
+
             local firstFree = nlx
             while (isSolid(firstFree, dy) or isSolid(firstFree, uy)) do
                 firstFree = firstFree + 1
@@ -101,6 +171,8 @@ return {
                 self.v.x = 0
             end
         elseif self.v.x > 0 then
+            self.direction = 1
+            
             local firstFree = nrx
             while (isSolid(firstFree, dy) or isSolid(firstFree, uy)) do
                 firstFree = firstFree - 1
@@ -144,10 +216,47 @@ return {
                 self.v.y = 0
             end
         end
+
+        -- Change animation
+        if self.state.grounded then
+            if self.v.x == 0 then
+                if self.direction == 1 then
+                    self.currAnimation = self.anims.idleRight
+                else
+                    self.currAnimation = self.anims.idleLeft
+                end
+            else
+                if self.direction == 1 then
+                    self.currAnimation = self.anims.moveRight
+                else
+                    self.currAnimation = self.anims.moveLeft
+                end
+            end
+        elseif self.state.climbing then
+            if self.direction == 1 then
+                self.currAnimation = self.anims.climbingRight
+            else
+                self.currAnimation = self.anims.climbingLeft
+            end
+        else
+            if self.v.y < 0 then
+                if self.direction == 1 then
+                    self.currAnimation = self.anims.risingRight
+                else
+                    self.currAnimation = self.anims.risingLeft
+                end
+            else
+                if self.direction == 1 then
+                    self.currAnimation = self.anims.fallingRight
+                else
+                    self.currAnimation = self.anims.fallingLeft
+                end
+            end
+        end
+
+        self.currAnimation:update(dt)
     end,
     draw = function(self)
-        g.setColor(100, 100, 100)
-        g.rectangle("fill", self.p.x, self.p.y, self.d.w, self.d.h)
-        g.setColor(255, 255, 255)
+        self.currAnimation:draw(self.spritesheet, self.p.x + (self.d.w - self.d.sprite.w) / 2, self.p.y + (self.d.h - self.d.sprite.h) / 2)
     end
 }
